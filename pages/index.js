75b59550d9a93edef39cfca071cc838febf32a30
Ciervos <55422681+Ciervos@users.noreply.github.com>
2020-10-21 21:59:16 -0300

    inicio dashboard blog

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,49 @@ import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import {useState} from 'react'
 
-export default function Home() {
-  const [texto,setTexto] = useState("default")
+
+
+
+export default function Home({blogs}){
+
+  return(<>
+  {blogs.map((blog,key)=>{
+    return(<div key={key}><h1>{blog.title}</h1> 
+    <h2>By {blog.user.username}</h2>
+      <p>{blog.description}</p></div>
+     )
+  })}
   
-  function handleChange(e){
-   setTexto(e.target.value)
-  } 
-
-
-  return (
-    <div className={styles.container}>
-      <input onChange={handleChange} type="text"/>
-      <Link href={`/prueba/${texto}`}>Click me</Link>
-    </div>
-  )
+  </>)
 }
+
+export async function getStaticProps(){
+  const data = await fetch("https://dev.to/api/articles?tag=javascript&top=1")
+  const json = await data.json();
+
+  return{
+    props:{
+      blogs: json,
+    }
+  };
+}
+
+
+
+// export default function Home() {
+
+  
+// //   const [texto,setTexto] = useState("default")
+  
+// //   function handleChange(e){
+// //    setTexto(e.target.value)
+// //   } 
+
+
+// //   return (
+// //     <div className={styles.container}>
+// //       <input onChange={handleChange} type="text"/>
+// //       <Link href={`/prueba/${texto}`}>Click me</Link>
+// //     </div>
+// //   )
+// // }
